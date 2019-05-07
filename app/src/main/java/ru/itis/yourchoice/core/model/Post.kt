8ea173a39362ba4b026a4605f7a7d3e0ac3b6b78
AlmazKh <32297268+AlmazKh@users.noreply.github.com>
2019-05-07 08:35:01 +0300

    Add builders into models

--- app/src/main/java/ru/itis/yourchoice/core/model/Post.kt
+++ app/src/main/java/ru/itis/yourchoice/core/model/Post.kt
@@ -6,14 +6,50 @@ import com.google.firebase.firestore.PropertyName
 data class Post(
     @get:PropertyName("subcategory_id")
     @set:PropertyName("subcategory_id")
-    var subcategoryId: Int,
+    var subcategory: Subcategory?,
     @get:PropertyName("name")
     @set:PropertyName("name")
-    var postName: String,
-    val description: String,
+    var postName: String?,
+    var description: String?,
     @get:PropertyName("owner_id")
     @set:PropertyName("owner_id")
-    var ownerId: String
+    var owner: User?
 ) {
-    constructor() : this(0, "", "", "")
+
+    constructor() : this(Subcategory(0, "", 0), "", "", User())
+
+    data class Builder(
+        var subcategory: Subcategory? = null,
+        var postName: String? = null,
+        var description: String? = null,
+        var owner: User? = null
+    ) {
+        fun subcategory(subcategory: Subcategory) = apply { this.subcategory = subcategory }
+        fun postName(postName: String?) = apply { this.postName = postName }
+        fun description(description: String?) = apply { this.description = description }
+        fun owner(owner: User?) = apply { this.owner = owner }
+        fun build() = Post(subcategory, postName, description, owner)
+    }
+
 }
+
+
+//class FoodOrder(
+//    val bread: String?,
+//    val condiments: String?,
+//    val meat: String?,
+//    val fish: String?) {
+//
+//    data class Builder(
+//        var bread: String? = null,
+//        var condiments: String? = null,
+//        var meat: String? = null,
+//        var fish: String? = null) {
+//
+//        fun bread(bread: String) = apply { this.bread = bread
+//            fun condiments(condiments: String) = apply { this.condiments = condiments }
+//            fun meat(meat: String) = apply { this.meat = meat }
+//            fun fish(fish: String) = apply { this.fish = fish }
+//            fun build() = FoodOrder(bread, condiments, meat, fish)
+//        }
+//    }
--- app/src/main/java/ru/itis/yourchoice/core/model/Subcategory.kt
+++ app/src/main/java/ru/itis/yourchoice/core/model/Subcategory.kt
@@ -1,7 +1,18 @@
 package ru.itis.yourchoice.core.model
 
 data class Subcategory(
-    val id: Int,
-    val name: String,
-    val categoryId: Int
-)
+        val id: Int?,
+        var name: String?,
+        var categoryId: Int?
+) {
+    data class Builder(
+        var id: Int? = null,
+        var name: String? = null,
+        var categoryId: Int? = null
+    ) {
+        fun id(id: Int?) = apply { this.id = id }
+        fun name(name: String?) = apply { this.name = name }
+        fun categoryId(categoryId: Int?) = apply { this.categoryId = categoryId }
+        fun build() = Subcategory(id, name, categoryId)
+    }
+}
